29ffb320b566d68fdd288dc0be2c82ddfe6b3822
[Ruliaga] <ruliaga@mail.ru>
2022-08-27 12:41:04 +0500

    Task 19 done

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число:");
+int number=Convert.ToInt32(Console.ReadLine());
+
+int check=number.ToString().Length;
+
+if (check==5) {
+
+char [] num = number.ToString().ToCharArray();
+
+if(num[0]==num[4] && num[1]==num[3]){
+    Console.WriteLine("это число палиндром");
+} else {
+    Console.WriteLine("это число НЕ палиндром");
+}
+
+    
+} else {
+
+Console.WriteLine("Вы ввели некорректное число.");
+
+}
