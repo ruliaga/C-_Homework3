273ecd1f6c3443155ff1225533f17d71e55c9a0b
[Ruliaga] <ruliaga@mail.ru>
2022-08-27 14:49:11 +0500

    Task23 done

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите N");
+int N=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("----------------------");
+
+Console.Write($"{N} -> ");
+
+double position=1.0;
+
+while (position<=N){
+    
+    Console.Write($"{Math.Pow(position,3.0)} ");
+    position++;
+}
+
