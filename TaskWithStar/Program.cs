fe051c6f07ecf4265006fa9cea90b3e3f193eb38
[Ruliaga] <ruliaga@mail.ru>
2022-08-27 23:06:19 +0500

    TaskWithStar done

--- TaskWithStar/Program.cs
+++ TaskWithStar/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите радиус круга: ");
+double R=Convert.ToDouble(Console.ReadLine());
+
+int S=Convert.ToInt32(Math.Round(Math.PI*R*R/2));
+
+char[] array=S.ToString().ToCharArray();
+
+int max=0;
+int index=0;
+
+while (index<array.Length){
+
+if((Convert.ToInt32(array[index]))>max){
+    max=Convert.ToInt32(array[index]);
+    index++;
+} else{
+
+    index++;
+}
+}
+Console.WriteLine(max);
